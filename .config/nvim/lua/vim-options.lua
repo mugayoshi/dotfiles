c3d464162f60cb4e791aff9201fa01a70a24a9d0
Muga <muga.y.j3039@gmail.com>
2024-05-12 21:27:40 +0200

    change tab setting for neovim

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,3 +1,7 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.cmd('inoremap <C-j> <Esc>')
 vim.cmd('inoremap <Esc> <Nop>')
 vim.g.mapleader = " "
