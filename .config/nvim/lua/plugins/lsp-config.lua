cdb8a2fa6368a17d0e67a3b5b6d010fd08bf3ae9
Muga <muga.y.j3039@gmail.com>
2024-05-13 11:42:07 +0200

    add go lsp config for neovim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
 	    require("mason-lspconfig").setup({
-	      ensure_installed = { "lua_ls" }
+	      ensure_installed = { "lua_ls", "gopls" }
     	})
     end
  },
@@ -17,12 +17,28 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local util = require("lspconfig/util")
 
 	    local lspconfig = require("lspconfig")
     	lspconfig.lua_ls.setup({})
 
       lspconfig.lua_ls.setup({
         capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = {"gopls"},
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
