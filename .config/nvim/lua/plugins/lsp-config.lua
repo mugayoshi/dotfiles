383ccb58778ff63f00dc104f8cdc2e1236d2a4d0
Muga <muga.y.j3039@gmail.com>
2024-05-14 17:22:12 +0200

    add python base config for Neovim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
 	    require("mason-lspconfig").setup({
-	      ensure_installed = { "lua_ls", "gopls" }
+	      ensure_installed = { "lua_ls", "gopls", "pyright" }
     	})
     end
  },
@@ -39,6 +39,10 @@ return {
             },
           },
         },
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        filetypes = { "python" }
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
