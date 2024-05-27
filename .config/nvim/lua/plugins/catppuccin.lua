c271c914b4cdb73c5f87332d51268b05471282a2
Muga Yoshikawa <muga.yoshikawa@chartboost.com>
2024-05-27 16:02:08 +0200

    add settings for line numbers

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,10 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin-mocha"
+
+      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+      vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
     end
   }
 }
