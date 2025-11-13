11430f59e41adc9d3d31c99a24f879fcace42f46
Muga Yoshhikawa <muga@mugas-macbook-air.home>
2025-11-13 23:22:25 +0100

    replace fugitive with neogit

--- .config/nvim/lua/plugins/git-config.lua
+++ .config/nvim/lua/plugins/git-config.lua
@@ -6,6 +6,19 @@ return {
     end
   },
   {
-    "tpope/vim-fugitive"
-  },
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  }
+  
 }
