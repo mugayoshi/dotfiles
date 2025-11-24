72be021515a5b1f6b2db69fb23c6e31d5b3ac431
Muga Yoshhikawa <muga@mugas-macbook-air.home>
2025-11-24 15:29:35 +0100

    Revert "Reapply "Merge branch 'add-neogit'""
    This reverts commit efde3fd5a10538c5e8ffe26ade8a564ef2ecc55c.

--- .config/nvim/lua/plugins/git-config.lua
+++ .config/nvim/lua/plugins/git-config.lua
@@ -6,19 +6,6 @@ return {
     end
   },
   {
-    "NeogitOrg/neogit",
-    lazy = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    cmd = "Neogit",
-    keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-    }
-  }
-  
+    "tpope/vim-fugitive"
+  },
 }
