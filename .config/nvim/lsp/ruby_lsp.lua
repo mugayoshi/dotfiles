b3e02a8f2f4c626060720d55de14132aab1f5557
Muga Yoshikawa <muga.yoshikawa@stockmark.co.jp>
2026-05-07 17:44:19 +0900

    add missing config for Rails + sorbet project

--- .config/nvim/lsp/ruby_lsp.lua
+++ .config/nvim/lsp/ruby_lsp.lua
@@ -1,9 +1,12 @@
 -- on Linux and macOS the default location is ~/.config/nvim/lsp/ruby-lsp.lua
 return {
   cmd = { "mise", "exec", "--", "ruby-lsp" },
+  cmd_env = {
+    -- see ruby_lsp_setup.md for the details
+    RUBY_LSP_BYPASS_TYPECHECKER = '1',
+  },
   filetypes = { "ruby", "eruby" },
   root_markers = { "Gemfile", ".ruby-version", ".mise.toml", ".git" },
-
   init_options = {
     formatter = 'auto',
     linters = { 'rubocop' },
