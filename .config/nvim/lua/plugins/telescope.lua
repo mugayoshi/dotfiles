2fb499086165610f0a2bdafa2f2109a31fd6fa5a
Muga Yoshikawa <muga.yoshikawa@stockmark.co.jp>
2026-05-14 16:18:27 +0900

    Neovim: add live_grep config to telescope so that hidden files can be searched.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,6 +26,20 @@ return {
             -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
             find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
           },
+          live_grep = {
+            vimgrep_arguments = {
+              "rg",
+              "--color=never",
+              "--no-heading",
+              "--with-filename",
+              "--line-number",
+              "--column",
+              "--smart-case",
+              "--hidden",
+              "--glob",
+              "!**/.git/*",
+            },
+          }
         },
         extensions = {
           ["ui-select"] = {
