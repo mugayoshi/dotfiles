761f02c2d2ec22a4117b6f4d18d3a05e5bf06f35
Muga Yoshikawa <muga.yoshikawa@stockmark.co.jp>
2026-05-15 17:35:21 +0900

    Neovim: render dot directories without dimming (except .git)

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -13,6 +13,8 @@ return {
       	  filesystem = {
             filtered_items = {
               visible = true,
+              hide_dotfiles = false,
+              hide_by_name = { ".git" },
             }
           },
         })
